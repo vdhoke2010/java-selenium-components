2e8483bb31137e5646d384357e487c09fa9ca8dc
Vibhay Dhoke <vdhoke2010@gmail.com>
2019-08-01 06:14:14 +0100

    added code for textbox

--- src/test/java/textbox/TextboxTest.java
+++ src/test/java/textbox/TextboxTest.java
@@ -1,11 +1,28 @@
 package textbox;
 
 import org.junit.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByName;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class TextboxTest {
 
 	@Test
 	public void verifyTextBox() {
+
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Java_my\\cucumber-java-vibhay\\java-selenium-command\\java-selenium-components\\src\\test\\resources\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://newtours.demoaut.com/mercurywelcome.php");
+		driver.manage().window().maximize();
+//		System.out.println("hellow to selenium");
+		By byName = By.name("userName");
+		
+		WebElement element = driver.findElement(byName);
+		
+		element.sendKeys("satya");
 		
 	}
 }
